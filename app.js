0920260bb0820e050382d6d2a31db06cc3dcb5f0
Mannan Alisco <amannan2132@gmail.com>
2026-07-28 03:09:03 +0600

    Create Express app instance

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("h");
+// Imports
+require("dotenv").config();
+const express = require("express");
+
+// Create Express App
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    throw error;
+  }
+
+  console.log(`Listening on port ${process.env.PORT}!`);
+});
